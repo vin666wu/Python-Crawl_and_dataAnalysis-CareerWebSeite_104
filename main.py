a472b5a7af9acb1a04ef2ea17a23f299e3963e93
vin666wu <hydedada0129@gmail.com>
2024-03-26 14:48:00 +0800

    2. looping and collecting pages

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ from IPython.display import clear_output
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0',      #browser
        	'Referer': 'https://www.104.com.tw/'}                                                                   #last browse url(page)
-url = 'https://www.104.com.tw/jobs/search/list?ro=0&area=6001001000&order=12&asc=0&page=2&mode=l'
-#get the data
-resp = requests.get(url, headers=headers)
-# DataFrame, JSON, first 3 rows
-df = pd.DataFrame(resp.json()['data']['list']).head(3)
+
+df = []
+
+for page in range(1,10):
+	url = f'https://www.104.com.tw/jobs/search/list?ro=0&area=6001001000&order=12&asc=0&page={page}&mode=l'
+	print(url)
+	resp = requests.get(url, headers=headers)
+	ndf = pd.DataFrame(resp.json()['data']['list'])
+	df.append(ndf)
+	if ndf.shape[0]<=30:
+		break
+df = pd.concat(df, ignore_index=True)
 print(df)
+# #get the data
+# resp = requests.get(url, headers=headers)
+# # DataFrame, JSON, first 3 rows
+# df = pd.DataFrame(resp.json()['data']['list']).head(3)
+# print(df)
